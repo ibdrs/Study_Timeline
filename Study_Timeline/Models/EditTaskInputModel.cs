61fdd83103bfa24990799c29bced32f37ecaeb7c
ibdrs <ivanbogdanovic677@gmail.com>
2025-12-09 20:11:33 +0100

    made custom properties for datetimes
    made it so that the max time value is minutes and not ms anymore, more user friendly

--- Study_Timeline/Models/EditTaskInputModel.cs
+++ Study_Timeline/Models/EditTaskInputModel.cs
@@ -2,10 +2,27 @@
 {
     public class EditTaskInputModel
     {
+        // backing fields for default time values
+        private DateTime _startTime = DateTime.Now;
+        private DateTime _endTime = DateTime.Now.AddHours(1);
+
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public DateTime StartTime { get; set; } = DateTime.Now;
-        public DateTime EndTime { get; set; } = DateTime.Now.AddHours(1);
+        public DateTime StartTime
+        {
+            get => new DateTime(_startTime.Year, _startTime.Month, _startTime.Day,
+                                _startTime.Hour, _startTime.Minute, 0);
+
+            set => _startTime = value;
+        }
+
+        public DateTime EndTime
+        {
+            get => new DateTime(_endTime.Year, _endTime.Month, _endTime.Day,
+                                _endTime.Hour, _endTime.Minute, 0);
+
+            set => _endTime = value;
+        }
         public int ProgressPercentage { get; set; }
     }
 }
